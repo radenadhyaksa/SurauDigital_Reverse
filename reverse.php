7d6f4fb1283944c680f91d7c5748d341e800acaf
Raden Adhyaksa <raksa69@gmail.com>
2018-08-16 20:34:32 +0700

    no message

--- reverse.php
+++ reverse.php
@@ -3,8 +3,8 @@ $set = "<pre>". "setyagus sucipto   sedang makan " ."</pre>";
 echo $set;
 
 echo "<br>";
-$ubah = array_reverse(explode(' ', $set));
-$set = join (' ', $ubah);
+$ubah = array_reverse(explode(' ' ,$set));
+$set = join (' ' ,$ubah);
 echo $set;
 
 ?>
