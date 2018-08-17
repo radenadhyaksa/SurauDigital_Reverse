5bcf2f7259678d88d2e005c88a37674420d92454
Raden Adhyaksa <raksa69@gmail.com>
2018-08-17 14:00:33 +0700

    add form

--- reverse.php
+++ reverse.php
@@ -1,10 +1,26 @@
-<?php
-$set =  "setyagus sucipto   sedang makan ";
-echo "<pre> $set </pre>";
+<html>
+   <body>
+     <div class="wrapper">
+       <form method="post">
+         <div>
+           <label>Masukkan Kalimat</label>
+           <input type="text" name="text"/>
+           <input type="submit" name="submit" value="kirim"/>
+         </div>
+       </form>
+     </div>
+   </body>
+</html>
 
-echo "<br>";
-$ubah = array_reverse(explode(' ' ,$set));
-$set = join (' ' ,$ubah);
-echo $set;
+<?php
+  if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $set = $_POST['text'];
+    echo "<pre> $set </pre>";
+    
+    echo "<br>";
+    $ubah = array_reverse(explode(" " ,$set));
+    $set = join (' ' ,$ubah);
+    echo $set;
+}
 
 ?>
